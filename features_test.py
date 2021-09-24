1c8a097d32d240696da3b1eefebdc05d1e7359de
Andrew Kiggins <30563055+Kiggins26@users.noreply.github.com>
2021-09-23 23:20:41 -0700

    Update features_test.py

--- features_test.py
+++ features_test.py
@@ -9,3 +9,10 @@ Write a test for each feature below
 
 def test_feature1():
     assert feature1("Teacher")=='T'
+    assert feature1("Teacher")=='T'
+def test_feature2():
+    assert feature2("Teacher")=='e'
+def test_feature3():
+    assert first_half("testtest")=="test"
+def test_feature4():
+    assert feature4("1111") == "11"
