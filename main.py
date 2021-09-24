68835faee1b6f278e9dce005ac03517024020c61
ramisanaima <ramisanaima@gmail.com>
2021-09-23 21:58:09 -0700

    Fix function

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ user_input  = input("enter your string: ")
 
 
 def first_half(input):
-    end = math.ceil(len(input)/2)
+    end = math.floor(len(input)/2)
     print(input[0:end])
 
 first_half(user_input)
