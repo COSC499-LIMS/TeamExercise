535f3cc8b411133ea5f6ed4991ae0cb3b474b01b
Andrew Kiggins <andrewkiggins2000@gmail.com>
2021-09-23 22:42:43 -0700

    fixed formatting problems added feature 4

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
-user_input  = input("enter your string: ")
-
+"""
+Team exercise git
+"""
 def feature1(word):  
-  first_char=user_input[0]
-     """
-    This feature returns the user the first letter in the word
     """
-  return first_char
+    This feature returns to the user the first letter in the word
+    """
+    first_char = user_input[0]
+    return first_char
 
 def feature2(word):
     """
-    This feature returns the user the second letter in the word
+    This feature returns to the user the second letter in the word
     """
     return word[1]
 
+def feature4(word):
+    """
+    This feature returns the user the second half of the word
+    """
+    half_index = int(len(word) / 2)
+    return  word[half_index:]
